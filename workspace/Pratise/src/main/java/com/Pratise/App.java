2b5f377e5c7ebeb2455aa6f7e807a41afb52d590
428148 <428148@FG2000103334.FutureGroup.net>
2017-12-28 19:53:26 +0530

    first commit

---
+++ workspace/Pratise/src/main/java/com/Pratise/App.java
@@ -0,0 +1,13 @@
+package com.Pratise;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
