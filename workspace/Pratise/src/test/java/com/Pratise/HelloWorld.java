ec70e779a67927a8e50390c13ac003a668da7e48
Saumya Panigrahy <saumyapanigrahy@FG2000121941.local>
2020-10-27 19:19:16 +0530

    Hello WOrld

--- workspace/Pratise/src/test/java/com/Pratise/HelloWorld.java
+++ workspace/Pratise/src/test/java/com/Pratise/HelloWorld.java
@@ -3,6 +3,6 @@ package com.Pratise;
 public class HelloWorld {
 public static void main(String[] args) {
 	System.out.println("hello world");
-	System.out.println("Love you soumya");
+	System.out.println("Ha ha ha Ha ha");
 }
 }
