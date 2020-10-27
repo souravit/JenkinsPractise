380f2cde9ce3fbdc1f2a13fa16f28c6b180c1b18
sourav singh <souravsingh@souravs-MacBook-Air.local>
2020-10-27 18:46:55 +0530

    done

--- workspace/Pratise/src/test/java/com/Pratise/HelloWorld.java
+++ workspace/Pratise/src/test/java/com/Pratise/HelloWorld.java
@@ -4,5 +4,8 @@ public class HelloWorld {
 public static void main(String[] args) {
 	System.out.println("hello world");
 	System.out.println("Love you soumya");
+	System.out.println("abc ");
+	System.out.println("xyz ");
+	
 }
 }
